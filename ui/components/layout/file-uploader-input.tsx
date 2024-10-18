274f3488796ca13a5a1e17cc9830f03e5351b4dc
JohnAdib <JohnAdib@users.noreply.github.com>
2024-10-18 01:26:04 +0000

    deploy: fd200e8c00655255c4fed175cda0d33b11a92f0d

--- ui/components/layout/file-uploader-input.tsx
+++ ui/components/layout/file-uploader-input.tsx
@@ -19,19 +19,15 @@ export function FileUploaderInput({ onFileSelect }: FileUploaderInputProps) {
     <div className="col-span-full select-none px-4 md:px-8">
       <label
         htmlFor="file-upload"
-        className="block text-sm font-medium leading-6 text-white"
-      ></label>
-      <div className="mt-2 flex justify-center rounded-lg border border-dashed border-white/25 hover:border-white/50 cursor-pointer transition px-4 py-6">
+        className="mt-2 flex justify-center rounded-lg border border-dashed border-black/25 dark:border-white/25 hover:border-black/50 dark:hover:border-white/50 cursor-pointer transition px-4 py-6"
+      >
         <div className="text-center">
           <ArrowUpTrayIcon
             aria-hidden="true"
             className="mx-auto h-10 w-10 text-gray-500"
           />
-          <div className="mt-4 flex text-sm leading-6 text-gray-400">
-            <label
-              htmlFor="file-upload"
-              className="relative cursor-pointer rounded-md bg-gray-900 font-semibold text-white focus-within:outline-none hover:text-sky-500"
-            >
+          <div className="mt-4 flex text-sm leading-6 text-gray-600 dark:text-gray-400">
+            <div className="relative cursor-pointer rounded-md font-semibold text-black dark:text-white focus-within:outline-none hover:text-sky-500 transition">
               <span>Upload a CSV file</span>
               <input
                 id="file-upload"
@@ -41,12 +37,14 @@ export function FileUploaderInput({ onFileSelect }: FileUploaderInputProps) {
                 className="sr-only"
                 onChange={handleFileChange}
               />
-            </label>
+            </div>
             <p className="pl-1">or drag and drop</p>
           </div>
-          <p className="text-xs leading-5 text-gray-400">CSV up to 1MB</p>
+          <p className="text-xs leading-5 text-gray-600 dark:text-gray-400">
+            CSV up to 1MB
+          </p>
         </div>
-      </div>
+      </label>
     </div>
   )
 }
